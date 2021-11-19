3d21e921a8550f52e2266b1fbd53403c09fe1a21
Hozenyth Andrade <hozenyth.andrade@gmail.com>
2021-11-19 20:57:01 -0300

    Ajustado Configurations

--- Data/LetsLikeContext.cs
+++ Data/LetsLikeContext.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using LetsLike.Configurations;
+using LetsLike.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,5 +10,34 @@ namespace LetsLike.Data
 {
     public class LetsLikeContext : DbContext
     {
+        // TODO instancia das models 
+        public DbSet<Usuario> Usuario { get; set; }
+        public DbSet<Projeto> Projetos { get; set; }
+
+        public LetsLikeContext(DbContextOptions<LetsLikeContext> options) : base(options)
+        {
+
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                var connection = @"Server=localhost\\SQLEXPRESS01;Database=LetsLike;Trusted_Connection=True;";
+
+                optionsBuilder.UseSqlServer(connection);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            // todo ApplyConfiguration aplica as configurações de entidade que criamos
+         
+            modelBuilder.ApplyConfiguration(new UsuarioConfiguration());
+            modelBuilder.ApplyConfiguration(new ProjetoConfiguration());
+        }
+
+
+
     }
 }
